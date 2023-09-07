6bd8c7b2ac749360f1282c0768bd0028a426c8e2
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2023-09-07 22:48:31 +0530

    Post & Get method addes

--- RestaurantApp/src/app/restaurant-dash/restaurant-dash.component.ts
+++ RestaurantApp/src/app/restaurant-dash/restaurant-dash.component.ts
@@ -11,25 +11,45 @@ import { ApiService } from '../shared/api.service';
 export class RestaurantDashComponent {
 
   formValue!: FormGroup;
-    name: string = '';
   restaurantModelObj: RestaurantData = new RestaurantData;
+  allRestaurantData: any;
+
   constructor(private formBuilder: FormBuilder , private api:ApiService) {}
 
   ngOnInit(): void {
     this.formValue = this.formBuilder.group({
+      id: [''],
       name: [''],
       email: [''],
       mobile: [''],
       address: [''],
-      services: [''],
+      services: ['']
     });
+
+    this.getAllData()
   }
 
   addRestro(){
+    // this.restaurantModelObj.id = this.formValue;
     this.restaurantModelObj.name = this.formValue.value.name;
     this.restaurantModelObj.email = this.formValue.value.email;
     this.restaurantModelObj.mobile = this.formValue.value.mobile;
     this.restaurantModelObj.address = this.formValue.value.address;
     this.restaurantModelObj.services = this.formValue.value.services;
+
+    this.api.postRestaurant(this.restaurantModelObj).subscribe((ref)=> {
+      console.log(ref);
+       this.getAllData()
+
+    });    
   }
+
+  getAllData(){
+    this.api.getRestaurant().subscribe((res) => {
+      this.allRestaurantData = res;
+      console.log('getdata',res)
+    })
+  }  
+
+
 }
